fa68e5d955da7681485e4e13932b62ad28d3686f
koll2222 <70836139+koll2222@users.noreply.github.com>
2023-04-05 20:57:14 +0900

    슬라임 점프, 플레이어 감지

--- AE/Assets/Script/Monster/MonsterMovement.cs
+++ AE/Assets/Script/Monster/MonsterMovement.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class MonsterMovement : MonsterProperty
 {
-    
+    // Get Target
+    protected void GetTarget(Collider2D col)
+    {
+        if (myTarget == null)
+        {
+            if( (1 << col.gameObject.layer & targetMask) != 0)
+                myTarget = col.transform;
+        }
+    }
+    // Lost Target
+    protected void LostTarget(Collider2D col)
+    {
+        myTarget = null;
+    }
+    // Target Tracing
 }
