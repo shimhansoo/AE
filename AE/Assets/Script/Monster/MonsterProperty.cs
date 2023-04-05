501a6d66d37b65c0a648db6ece99915b7645de6a
koll2222 <70836139+koll2222@users.noreply.github.com>
2023-04-05 20:32:57 +0900

    플레이어 Trace 제작 중, Test 맵, 몬스터 프로퍼티, 무브먼트 기본적인 세팅

---
+++ AE/Assets/Script/Monster/MonsterProperty.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterProperty : MonoBehaviour
+{
+    public float MoveSpeed = 1f;    // 몬스터의 이동 속도
+    Transform myTarget = null; // 타겟 저장
+    public LayerMask targetMask;   // 타겟 레이어 지정
+
+    // Get Rigidbody2D
+    Rigidbody2D _rigid;
+    protected Rigidbody2D myRigid
+    {
+        get
+        {
+            if(_rigid == null)
+            {
+                _rigid = GetComponent<Rigidbody2D>();
+                if(_rigid == null)
+                {
+                    _rigid = GetComponentInChildren<Rigidbody2D>();
+                }
+            }return _rigid;
+        }
+    }
+
+    // Get Animator
+    Animator _anim;
+    protected Animator myAnim
+    {
+        get
+        {
+            if(_anim == null)
+            {
+                _anim = GetComponent<Animator>();
+                if(_anim == null)
+                {
+                    _anim = GetComponentInChildren<Animator>();
+                }
+            }return _anim;
+        }
+    }
+}
