9b6e006e385d75c8b8bb98a4b62d1e19b39c9f29
SangYuNam <128944348+SangYuNam@users.noreply.github.com>
2023-05-22 20:04:35 +0900

    아이템 끝 ㅋ

--- AE/Assets/Script/Boss/Dropfire.cs
+++ AE/Assets/Script/Boss/Dropfire.cs
@@ -23,7 +23,7 @@ public class Dropfire : Boss2Property
     {
        
         StartCoroutine(Moving());
-        this.attackDamage = myParent.SkillDamage ;
+        /*this.attackDamage = myParent.SkillDamage;*/
         transform.SetParent(null);
       
        
